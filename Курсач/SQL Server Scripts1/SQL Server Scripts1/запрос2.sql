d12ebb9694dff4cac72627adf7a1ff35b9f8c9aa
bestrizhiy <78790397+bestrizhiy@users.noreply.github.com>
2021-11-07 19:29:36 +0300

    Загрузил курсовой проект

---
+++ Курсач/SQL Server Scripts1/SQL Server Scripts1/запрос2.sql
@@ -0,0 +1,8 @@
+-- Выдавать список стран, популярных у определенного возраста клиентов.
+SELECT DISTINCT Countries.[Name] AS Country_Name
+FROM Countries
+INNER JOIN Tours
+ON Tours.ID_Country = Countries.ID
+INNER JOIN Clients
+ON Tours.ID_Client = Clients.ID
+WHERE Clients.Age BETWEEN 14 AND 25; 
---
+++ Курсач/SQL Server Scripts1/SQL Server Scripts1/запрос5.sql
@@ -0,0 +1,18 @@
+--Показать все цены на путёвки (без скидок) в порядке убывания.
+SELECT DISTINCT Countries.[Name] AS Country_Name
+	 , Cities.[Name] AS City_Name
+	 , Programs.[Name] AS [Program_Name]
+	 , Price
+FROM Tours
+INNER JOIN Countries
+ON Tours.ID_Country = Countries.ID
+INNER JOIN Cities
+ON Tours.ID_City = Cities.ID
+INNER JOIN Programs
+ON Tours.ID_Program = Programs.ID
+INNER JOIN Clients
+ON Tours.ID_Client = Clients.ID
+INNER JOIN Discounts
+ON Clients.ID_Discount = Discounts.ID
+WHERE Discounts.[Name] LIKE '%нет%' 
+ORDER BY Price DESC;
